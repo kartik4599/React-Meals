000f76f98b08db2ae208f552d2448c13f3df3c4e
devloper-kartik <kartikmendu2@gmail.com>
2022-10-02 11:35:20 +0530

    Cart Page

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,29 @@
+import classes from "./Modal.module.css";
+import ReactDOM  from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  const portal = document.getElementById("overlays");
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portal)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portal
+      )}
+    </>
+  );
+};
+
+export default Modal;
