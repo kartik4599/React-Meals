cf14999405d6c16d5c18183925e405834f7c349d
devloper-kartik <kartikmendu2@gmail.com>
2022-10-03 20:14:38 +0530

    Context Header

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,16 +1,21 @@
-// import { useContext } from "react";
+import { useContext } from "react";
+import CartContext from "../../Context/cart-context";
 import CartIcon from "../UI/Icon/CartIcon";
 import classes from "./HeaderCart.module.css";
 
 const HeaderCart = (props) => {
-  // const cxt=useContext();
+  const cxt = useContext(CartContext);
+
+  const noOfItems = cxt.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onshowCart}>
-      <span >
+      <span>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>0</span>
+      <span className={classes.badge}>{noOfItems}</span>
     </button>
   );
 };
