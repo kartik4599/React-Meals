7fe0bdc7879dcbbf7aaebd15a168444f4007bc3f
devloper-kartik <kartikmendu2@gmail.com>
2022-10-04 08:28:17 +0530

    bump Cart

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,16 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../Context/cart-context";
 import CartIcon from "../UI/Icon/CartIcon";
 import classes from "./HeaderCart.module.css";
 
 const HeaderCart = (props) => {
+  const [bump, setbump] = useState(false);
   const cxt = useContext(CartContext);
 
   const noOfItems = cxt.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${bump && classes.bump}`;
+  useEffect(() => {
+    if (cxt.items.length === 0) return;
+    setbump(true);
+    const timer= setTimeout(()=>setbump(false),300);
+
+    return()=>{
+      clearTimeout(timer);
+    }
+  }, [cxt.items]);
+
   return (
-    <button className={classes.button} onClick={props.onshowCart}>
+    <button className={btnClasses} onClick={props.onshowCart}>
       <span>
         <CartIcon />
       </span>
